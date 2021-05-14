ae277829dd012ea36dbe59167f5de5f7135f3b21
Harshit Varma <varmaharshit2@gmail.com>
2021-05-14 16:36:19 +0530

    Updated code

--- data/extract_topics.py
+++ data/extract_topics.py
@@ -143,27 +143,8 @@ class TopicExtractor:
 
 if __name__ == '__main__':
 
-    text = """
-        Whose woods these are I think I know.   
-        His house is in the village though;   
-        He will not see me stopping here   
-        To watch his woods fill up with snow.   
-
-        My little horse must think it queer   
-        To stop without a farmhouse near   
-        Between the woods and frozen lake   
-        The darkest evening of the year.   
-
-        He gives his harness bells a shake   
-        To ask if there is some mistake.   
-        The only other sound's the sweep   
-        Of easy wind and downy flake.   
-
-        The woods are lovely, dark and deep,   
-        But I have promises to keep,   
-        And miles to go before I sleep,   
-        And miles to go before I sleep.
-    """
+    with open("sample.txt", "r", encoding = "utf-8") as f:
+        text = f.read()
 
     from string import punctuation
     
